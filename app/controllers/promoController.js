b5178a14536e595f2daf61e07ba9b21b8703c3f0
Mathieu Noel <yzorien@gmail.com>
2022-05-11 23:51:07 +0200

    bonus2 done

--- app/controllers/promoController.js
+++ app/controllers/promoController.js
@@ -31,7 +31,7 @@ const promoController = {
   // },
   getPromosList(req, res ) {
     
-    const query = "SELECT name FROM promo ORDER BY name ASC";
+    const query = "SELECT * FROM promo ORDER BY name ASC";
     client
   .query(query)
   .then(results => {
@@ -51,15 +51,15 @@ const promoController = {
   getPromoDetails(req, res, next) {
     // on récupére la valeur de l'id contenu dans la route
     const promoId = req.params.id;
-    // on cherche dans le tableau des promos celle dont l'id est égal à l'id reçu via l'url
-    const promo = promos.find((promo) => promo.id === +promoId);
-    if (promo) {
-      // si la promo existe, on renvoie la page de détail
-      res.render("promoDetails", { promo });
-    } else {
-      // si la promo n'existe pas, on passe au middleware suivant
-      next();
-    }
+    const query =`SELECT * FROM student WHERE id=${promoId}`;
+    
+    client
+  .query(query) 
+  .then(results => {  
+    // si la promo existe, on renvoie la page de détail
+    res.render("promoDetails", { promo : results.rows[0]});      
+  })
+    .catch(e => console.error(e.stack)) 
   },
   /**
    * Display promo students list page
@@ -71,19 +71,14 @@ const promoController = {
     // on récupére la valeur de l'id contenu dans la route
     const promoId = Number(req.params.id);
     // on récupérer la promo qui a cet Id
-    const promo = promos.find((promo) => promo.id === promoId);
+    const query =`SELECT first_name, last_name, profile_picture_url, promo_id, github_username FROM student WHERE promo_id=${promoId}`;
+    // UNION ALL SELECT name, id FROM promo WHERE id=${promoId}
+    console.log(query)
+    
     // si j'ai bien récupérer une promo
-    if (promo) {
-      // je récupére tous les étudiants de cette promo
-      const studentsOfPromo = students.filter((student) => student.promo === promoId);
-      // et je renvoie le rendu du template promoStudents
-      res.render("promoStudents", {promo, students: studentsOfPromo});
-    } else {
-      // sinon je passe au middleware suivant
-      next();
-    }
-  }
-};
-
+    
+    client.query(query).then(results => {res.render("promoStudents",{promo : results.rows, students: results.rows})}).catch(e => console.error(e.stack))  
+  },
+}
 // on exporte le controller
 module.exports = promoController;
